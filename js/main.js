e1da15fe07ce82550721653a7740d460a359efeb
Ksmike <michael.copeland@news.co.uk>
2016-06-17 00:26:55 +0100

    Game: Fixed spent cell click interaction && added turn limit

--- js/main.js
+++ js/main.js
@@ -1,47 +1,52 @@
 $(function() {
-    var count = 0;
+    var count = 10;
     var audioElement = document.createElement('audio');
 
     audioElement.setAttribute('src', 'assets/grunt.mov');
 
-	$('.ready').click(function () {
+	$('body').on('click', '.ready', function () {
 
-        var newNm = Math.floor(Math.random() * (35 - 0 + 1)) + 0;
+        var newNm = Math.floor(Math.random() * (36 - 0 + 1)) + 0;
         console.log('newNm: ', newNm);
         if (!gameHasBeenWon()) {
-        	if (newNm !== 36) {
+            if (count !== 0) {
+                if (newNm !== 36) {
 
-        		if ($('.notCell.ready').length === 1) {
-        			$(this).removeClass('ready');
-        			$(this).addClass('won');
+                    if ($('.notCell.ready').length === 1) {
+                        $(this).removeClass('ready');
+                        $(this).addClass('won');
 
+                        audioElement.play();
+
+                    } else {
+                        $(this).removeClass('ready');
+                        $(this).addClass('spent');
+                    }
+                } else {
+                    $(this).removeClass('ready');
+                    $(this).addClass('won');
                     audioElement.play();
 
-        		} else {
-        			$(this).removeClass('ready');
-        			$(this).addClass('spent');
-        		}
-        	} else {
-        		$(this).removeClass('ready');
-        		$(this).addClass('won');
-                audioElement.play();
+                    alert('You Won!');
+                }
+                count--;
+                $("#counter").html("Turns left " + count + "!");
+            } else {
+                alert('LOSER!');
+                count = 0;
+                $("#counter").html("Try Again!");
+            }
 
-        		alert('You Won!');
-        	}
         } else {
         	alert('You found the treasure');
         }
-
-        count++;
-        $("#counter").html("Turn " + count + "!");
-
 	});
 
 	$('#reset').click(function () {
 
 		$('.notCell.won').removeClass('won').addClass('ready');
 		$('.notCell.spent').removeClass('spent').addClass('ready');
-        count = 0;
+        count = 10;
         $("#counter").html("New Game!");
 
 	});
@@ -50,8 +55,6 @@ $(function() {
     	if ($('.notCell.won').length === 1) {
     		return true;
     	}
-
     	return false;
-
 	}
 });
